2b23a4a651ea35fd631e79d08dcc0b7496f60072
OfficialScragg <danielscragg@outlook.com>
2021-10-18 19:30:08 +0200

    Soft-UI Theme
    Thank you Creative Tim.

--- Django/App/views.py
+++ Django/App/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def dashboard(request):
+    return render(request, "dashboard.html")
+
+def profile(request):
+    return render(request, "profile.html")
