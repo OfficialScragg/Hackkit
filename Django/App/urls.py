0167f8f5071b91623b1bb36472681a73a844f187
OfficialScragg <danielscragg@outlook.com>
2021-10-19 13:35:10 +0200

    Basic Website Layout

--- Django/App/urls.py
+++ Django/App/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('',views.index, name='index'),
     path('dashboard',views.dashboard, name='dashboard'),
     path('profile',views.profile, name='profile'),
+    path('decode',views.decoder, name='decoder'),
+    path('wordlists',views.wordlists, name='wordlists'),
+    path('cve',views.cve, name='cve'),
+    path('convert',views.convert, name='convert'),
 ]
--- Django/App/views.py
+++ Django/App/views.py
@@ -9,3 +9,15 @@ def dashboard(request):
 
 def profile(request):
     return render(request, "profile.html")
+
+def decoder(request):
+    return render(request, "decoder.html")
+
+def wordlists(request):
+    return render(request, "wordlists.html")
+
+def cve(request):
+    return render(request, "cve_search.html")
+
+def convert(request):
+    return render(request, "convert.html")
